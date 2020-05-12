07594e6ef9af17c0b0505265e47c6dec967a3528
Cook1619 <cook6348@gmail.com>
2020-05-12 16:48:55 -0500

    Removed derp

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyBQ2TdVBdf-MVbVQWXrW-MPssQW5PbWcZQ",
-    // authDomain: "dev-tips-n-tricks.firebaseapp.com",
-    // databaseURL: "https://dev-tips-n-tricks.firebaseio.com",
-    // projectId: "dev-tips-n-tricks",
-    // storageBucket: "dev-tips-n-tricks.appspot.com",
-    // messagingSenderId: "810173801564",
-    // appId: "1:810173801564:web:1257d16f0b339e97b43299",
-    // measurementId: "G-XCYLPESGSF"
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATBASEURL,
