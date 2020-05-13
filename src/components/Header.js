a448f96a5aa74ebe308e4af5569a1c4e66a05269
Cook1619 <cook6348@gmail.com>
2020-05-12 20:29:41 -0500

    Adding logo and commiting before blowing everything up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../mcook.png";
+import '../App.css'
 
 const Header = () => (
   <div>
     <header>
+      <img className="app-logo" src={logo} alt="logo" />
       <h1>Dev Tips and Tricks</h1>
       <NavLink to="/" exact={true}>
         Home
